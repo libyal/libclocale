b3d7562d660ccf48847b123bd803b17cb7cd1153
Joachim Metz <joachim.metz@gmail.com>
2012-09-19 21:18:11 +0200

    Changes of version: 20120919

--- libclocale/libclocale_definitions.h
+++ libclocale/libclocale_definitions.h
@@ -34,11 +34,11 @@
  */
 #else
 
-#define LIBCLOCALE_VERSION			20120904
+#define LIBCLOCALE_VERSION			20120919
 
 /* The libclocale version string
  */
-#define LIBCLOCALE_VERSION_STRING		"20120904"
+#define LIBCLOCALE_VERSION_STRING		"20120919"
 
 #endif
 
