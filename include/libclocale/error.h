cc2fd1ac79c50d59ef0c21362c2fae670fc74446
Joachim Metz <joachim.metz@gmail.com>
2014-03-26 07:46:35 +0100

    2014 update

--- include/libclocale/error.h
+++ include/libclocale/error.h
@@ -1,7 +1,7 @@
 /*
  * The error code definitions for libclocale
  *
- * Copyright (c) 2010-2013, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (c) 2010-2014, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
