be303ffbedb8723b04a2287b1cf7e6030b5b11e0
Joachim Metz <joachim.metz@gmail.com>
2016-02-14 16:59:48 +0100

    Code clean up.

--- libclocale/libclocale_libcstring.h
+++ libclocale/libclocale_libcstring.h
@@ -45,7 +45,7 @@
 
 #include <libcstring.h>
 
-#endif
+#endif /* defined( HAVE_LOCAL_LIBCSTRING ) */
 
-#endif
+#endif /* !defined( _LIBCLOCALE_LIBCSTRING_H ) */
 
--- libclocale/libclocale_unused.h
+++ libclocale/libclocale_unused.h
@@ -40,5 +40,5 @@
 	/* parameter */
 #endif
 
-#endif
+#endif /* !defined( _LIBCLOCALE_INTERNAL_UNUSED_H ) */
 
