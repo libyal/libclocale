d471e7d8f2591e2d0474ccd9afde9b0e5c107419
Joachim Metz <joachim.metz@gmail.com>
2019-11-13 08:29:07 +0100

    Applied updates

--- .travis/script.sh
+++ .travis/script.sh
@@ -25,7 +25,7 @@ then
 
 		make install DESTDIR=${PWD}/osx-pkg;
 		mkdir -p ${PWD}/osx-pkg/usr/share/doc/libclocale;
-		cp AUTHORS COPYING NEWS README ${PWD}/osx-pkg/usr/share/doc/libclocale;
+		cp AUTHORS COPYING COPYING.LESSER NEWS README ${PWD}/osx-pkg/usr/share/doc/libclocale;
 
 		pkgbuild --root osx-pkg --identifier com.github.libyal.libclocale --version ${VERSION} --ownership recommended ../libclocale-${VERSION}.pkg;
 	fi
