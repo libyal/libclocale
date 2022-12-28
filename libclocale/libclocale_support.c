e1b798b0a9e4920a655a8a32f355ef885a1035a6
Joachim Metz <joachim.metz@gmail.com>
2022-12-28 08:43:03 +0100

    Applied updates and changes for CI tests

--- libclocale/libclocale_support.c
+++ libclocale/libclocale_support.c
@@ -68,7 +68,7 @@ int libclocale_initialize(
 
 		return( -1 );
 	}
-#if defined( HAVE_BINDTEXTDOMAIN ) && defined( HAVE_TEXTDOMAIN )
+#if defined( HAVE_BINDTEXTDOMAIN ) && defined( HAVE_TEXTDOMAIN ) && defined( LOCALEDIR )
 	if( bindtextdomain(
 	     domain_name,
 	     LOCALEDIR ) == NULL )
@@ -94,7 +94,7 @@ int libclocale_initialize(
 
 		return( -1 );
 	}
-#endif /* defined( HAVE_BINDTEXTDOMAIN ) && defined( HAVE_TEXTDOMAIN ) */
+#endif /* defined( HAVE_BINDTEXTDOMAIN ) && defined( HAVE_TEXTDOMAIN ) && defined( LOCALEDIR ) */
 
 #if !defined( HAVE_WIDE_SYSTEM_CHARACTER )
 	if( libclocale_locale_get_codepage(
