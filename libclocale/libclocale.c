47af98b86a4b8a8351480a3b88eaf450259f2ff1
Joachim Metz <joachim.metz@gmail.com>
2025-11-26 05:24:30 +0100

    Applied updates

--- libclocale/libclocale.c
+++ libclocale/libclocale.c
@@ -1,7 +1,7 @@
 /*
  * Library to support cross-platform C locale functions
  *
- * Copyright (C) 2010-2024, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2025, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
