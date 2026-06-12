d77f78bcded8be92302e4982845334a56f806fe9
Joachim Metz <joachim.metz@gmail.com>
2026-06-12 12:23:29 +0200

    Replaced test scripts with autotest

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20201121
+# Version: 20260609
 
-if test -f ${PWD}/libclocale/.libs/libclocale.1.dylib && test -f ./pyclocale/.libs/pyclocale.so;
+if test -f ${PWD}/libclocale/.libs/libclocale.1.dylib && test -f ./pyclocale/.libs/pyclocale.so
 then
-	install_name_tool -change /usr/local/lib/libclocale.1.dylib ${PWD}/libclocale/.libs/libclocale.1.dylib ./pyclocale/.libs/pyclocale.so;
+	install_name_tool -change /usr/local/lib/libclocale.1.dylib ${PWD}/libclocale/.libs/libclocale.1.dylib ./pyclocale/.libs/pyclocale.so
 fi
 
-make check CHECK_WITH_STDERR=1;
-RESULT=$?;
+make check-build > /dev/null
 
-if test ${RESULT} -ne 0 && test -f tests/test-suite.log;
+make check $@
+RESULT=$?
+
+if test ${RESULT} -ne 0
 then
-	cat tests/test-suite.log;
+	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
-exit ${RESULT};
+exit ${RESULT}
 
