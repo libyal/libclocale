3e47216eb5161781ae03fcbc23160256426e7f9c
Joachim Metz <joachim.metz@gmail.com>
2026-09-14 10:58:05 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libclocale/.libs/libclocale.1.dylib && test -f ./pyclocale/.libs/pyclocale.so
+if [ -f "${PWD}/libclocale/.libs/libclocale.1.dylib" ] && [ -f ./pyclocale/.libs/pyclocale.so ]
 then
-	install_name_tool -change /usr/local/lib/libclocale.1.dylib ${PWD}/libclocale/.libs/libclocale.1.dylib ./pyclocale/.libs/pyclocale.so
+    install_name_tool -change /usr/local/lib/libclocale.1.dylib "${PWD}/libclocale/.libs/libclocale.1.dylib" ./pyclocale/.libs/pyclocale.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
